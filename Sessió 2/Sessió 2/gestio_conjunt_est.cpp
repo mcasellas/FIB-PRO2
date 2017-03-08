ebf921f49d5f3cb09ab6e5429d2b7f35e6545b71
mcasellas <marccasellas@iMac-de-Marc.local>
2017-03-08 22:38:32 +0100

    Entrada estudiant // consulta nota

--- Sessió 2/Sessió 2/gestio_conjunt_est.cpp
+++ Sessió 2/Sessió 2/gestio_conjunt_est.cpp
@@ -14,10 +14,7 @@ int main() {
     //Introdueix el conjunt
     cout << "Introdueix el conjunt:" << endl;
     cest.llegir();
-    
-    cout << "Introdueix l'ordre (-1 fins a -5)" << endl;
-    cin >> op;
-    /*
+        /*
      anadir un nuevo estudiante (opcion -1)
      consultar la nota de un estudiante a partir de su DNI (opcion -2)
      modificar la nota de un estudiante (opcion -3)
@@ -25,32 +22,42 @@ int main() {
      escribir el conjunto de estudiantes (opcion -5)
      */
     
+    cout << "Introdueix l'ordre (-1 fins a -5)" << endl;
     
-    
-    if (op == -1) {
-        Estudiant est;
+    while (cin >> op) {
+   
         
-        est.llegir();
-        cest.afegir_estudiant(est);
-    }
-    
-    else if (op == -2) {
-        cout << "Introdueix el DNI:" << endl;
-        int dni;
-        cin >> dni;
-        cout << cest.consultar_estudiant(dni).consultar_nota() << endl;
-    }
-    
-    else if (op == -3) {
+
         
-    }
-    
-    else if (op == -4) {
+        if (op == -1) { //entra un nou estudiant
+            Estudiant est;
+            cout << "Entra el DNI i la nota:" << endl;
+            est.llegir();
+            cest.afegir_estudiant(est);
+        }
         
-    }
-    
-    else {
-        cest.escriure();
+        else if (op == -2) { //consulta la nota d'un estudiant a través del DNI
+            cout << "Introdueix el DNI de l'estudiant:" << endl;
+            int dni;
+            cin >> dni;
+            
+            cout << cest.consultar_estudiant(dni).consultar_nota() << endl;
+        }
+        
+        else if (op == -3) { // modifica la nota d'un estudiant
+            
+        }
+        
+        else if (op == -4) { // Arrodonir la nota
+            
+        }
+        
+        else { // Escriure tots els elements
+            cest.escriure();
+        }
+        
+        cout << "Introdueix l'ordre (-1 fins a -5)" << endl;
+        
     }
     
     
